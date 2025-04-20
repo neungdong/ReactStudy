55c62b57dc35eb3c180205b51c9985f3410f1bb7
neungdong <iyeseul877@gmail.com>
2025-04-20 16:26:49 +0900

    Card 컴포넌트 재사용해서 OrderStatusCard 생성

--- src/pages/OrderPage/index.jsx
+++ src/pages/OrderPage/index.jsx
@@ -1,11 +1,29 @@
 import Navbar from "@/components/Navbar";
+import OrderStatusCard from "@/pages/OrderPage/OrderStatusCard";
 import Page from "@/components/Page";
 import Title from "@/components/Title";
+
+const fakeOrder = {
+  id: "CACDA420",
+  orderDate: "2023. 5. 13. 오후 1:37:51",
+  status: "음식 준비중",
+  name: "짜장면",
+  totalPrice: 7000,
+  paymentMethod: "마이페이",
+  productPrice: 6000,
+  deliveryPrice: 3000,
+  discountPrice: 2000,
+  deliveryAddress: "서울특별시 송파구 잠실동 1번지",
+  deliveryContact: "010-1111-2222",
+  messageToShop: "포크는 주지 마세요",
+  messageToRider: "안전하게 오세요",
+  position: [30, 30],
+};
 
 const OrderPage = () => (
   <div className="OrderPage">
     <Page header={<Title>주문내역</Title>} footer={<Navbar />}>
-      여기에 내용
+      <OrderStatusCard order={fakeOrder} />
     </Page>
   </div>
 );
